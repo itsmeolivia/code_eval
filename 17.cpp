5d96420066cffe31b6a123deb0ace3d7b860eb0a
itsmeolivia <nolivia@umich.edu>
2014-11-11 10:58:41 -0500

    17, i had this in a job interview
    not completely correct and not sure why

---
+++ 17.cpp
@@ -0,0 +1,47 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    ifstream file;
+    file.open(argv[1]);
+    
+    string line;
+    int num;
+    vector<int> array;
+    
+    while(file.good()){
+        getline(file, line);
+        if (line.size() == 0)
+            continue;
+        cout << line << endl;
+        
+        replace(line.begin(), line.end(), ',', ' ');
+        istringstream iss(line);
+        array.clear();
+        
+        int total;
+        int large = -1000;
+        
+        while (iss >> num)
+            array.push_back(num);
+        
+        for(unsigned int i = 0; i < array.size(); i++) {
+            total = 0;
+            for (unsigned int j = i; j < array.size(); j++)
+                total += array[j];
+                if (total > large)
+                    large = total;
+        }
+        cout << large << "\n";
+        
+    }
+    
+    return 0;
+}
