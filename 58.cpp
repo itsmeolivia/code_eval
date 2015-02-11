72057362e4ec731a46ea588f246e4f16ac09b798
itsmeolivia <nolivia@umich.edu>
2015-02-11 15:14:46 -0500

    85/100

--- 58.cpp
+++ 58.cpp
@@ -15,6 +15,7 @@ int main(int argc, char** argv) {
   string line;
   vector<vector <string> > source;
   set<string> dict;
+  set<string> copy;
 
   bool split = false;
   int i = 0;
@@ -36,27 +37,65 @@ int main(int argc, char** argv) {
     }
   }
 
-  for(int row = 0; row < i; row++) {
+  for(unsigned int row = 0; row < source.size(); row++) {
 
-    for(int col = 0; col < source[row].size(); col++) {
+    copy = dict;
 
+    for(unsigned int col = 0; col < source[row].size(); col++) {
+      string cur;
+
+      for (unsigned int k = 0; k < source[row][col].size(); k++) {
+
+        //substitute
+        cur = source[row][col];
+
+        for (int m = 0; m < 26; m++) {
+          cur[k] = 'a' + m;
+          set<string>::iterator it = copy.find(cur);
+          if (it != copy.end()) {
+            //note: doesnt check if the word can be a friend with itself?
+            copy.erase(it);
+            source[row].push_back(cur);
+          }
+
+        }
+      }
       
+      //add
+      for (unsigned int k = 0; k < source[row][col].size(); k++) {
+
+        cur = source[row][col];
+        cur.insert(k, "a");
+
+        for (int m = 0; m < 26; m++) {
+          cur[k] = 'a' + m;
+          set<string>::iterator it = copy.find(cur);
+
+          if (it != copy.end()) {
+            copy.erase(it);
+            source[row].push_back(cur);
+          }
+        }      
+      }
+
+      //subtract
+
+      for (unsigned int k = 0; k < source[row][col].size(); k++) {  
+        cur = source[row][col];
+        cur.erase(k, 1);
+
+        set<string>::iterator it = copy.find(cur);
+
+        if (it != copy.end()) {
+          copy.erase(it);
+          source[row].push_back(cur);
+        }
+
+      }
     }
-
-    //substitute
-
-
-    //add
-
-    //subtract
-
 
     cout << source[row].size() - 1 << endl;
   }
-
-
-
-
 
   return 0;
 }
