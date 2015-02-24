a0c97935168a3452e969c07c653851a9039f6bde
itsmeolivia <nolivia@umich.edu>
2015-02-24 17:01:53 -0500

    yay 100

--- 45.cpp
+++ 45.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
+
+long reverse(long x) {
+   long result = 0; 
+   while (x > 0) {
+        result *= 10;
+        result += (x % 10);
+        x /= 10;   
+	}
+	return result;
+}
+
+bool isPalindrome(long x) {
+	vector<int> v;
+	v.clear();
+
+	while (x > 0) {
+		v.push_back(x%10);
+		x /= 10;
+	}
+
+	for (unsigned int i = 0; i < v.size()/2; i++) {
+		if (v[i] != v[v.size()- i - 1])
+			return false;
+	}
+	return true;
+}
 
 int main(int argc, char** argv) {
 
@@ -11,9 +37,17 @@ int main(int argc, char** argv) {
   file.open(argv[1]);
   
   while (file.good()) {
-  	int num;
-  	num >> file;
+  	long num;
+  	file >> num;
+
+  	int count = 0;
+  	while (!isPalindrome(num)) {
+  		count++;
+  		num = num + reverse(num);
+  	}
+
+  	cout << count << " " << num << "\n";
   	
   }
-
+  return 0;
 }
